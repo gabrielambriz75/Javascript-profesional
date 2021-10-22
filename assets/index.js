f859829b9c67816589d5104ac0b5cd660c705fae
Angel Gabriel Dominguez Ambriz <angelgabrielda@hotmail.com>
2021-10-22 18:43:44 -0500

    8 - This

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,16 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector("video");
 const button = document.querySelector("button");
+const muteButton = document.querySelector("#un-mute");
 
 const player = new MediaPlayer({
-    el: video
+    el: video,
+    plugins: [
+        // new AutoPlay()
+    ]
 });
 
 button.onclick = () => player.togglePlay();
+muteButton.onclick = () => player.toggleMute();
