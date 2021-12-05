c3fc32d4c49a95fd8deb6d738c45f6504927f4c5
Gabriel Ambriz <angelgabrielda@hotmail.com>
2021-12-04 18:49:03 -0600

    17 - getters y setters

--- assets/index.js
+++ assets/index.js
@@ -8,7 +8,7 @@ const muteButton = document.querySelector("#un-mute");
 const player = new MediaPlayer({
     el: video,
     plugins: [
-        // new AutoPlay()
+        new AutoPlay()
     ]
 });
 
