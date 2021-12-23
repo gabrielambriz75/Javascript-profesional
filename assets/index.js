e7b050a51b5620aed09fae41df1935aadee8a464
Gabriel Ambriz <angelgabrielda@hotmail.com>
2021-12-23 00:25:37 -0600

    23 - Service Workers

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,21 @@
-import MediaPlayer from './MediaPlayer.js';
-import AutoPlay from './plugins/AutoPlay.js';
-import AutoPause from './plugins/AutoPause.js';
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
 
 const video = document.querySelector("video");
 const button = document.querySelector("button");
 const muteButton = document.querySelector("#un-mute");
 
 const player = new MediaPlayer({
-    el: video,
-    plugins: [
-        new AutoPlay(),
-        new AutoPause()
-    ]
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
 });
 
 button.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").catch((error) => {
+    console.log(error.message);
+  });
+}
