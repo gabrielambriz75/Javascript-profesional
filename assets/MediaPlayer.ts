5cd7765bb82a296f2ccf8da86cea74f3cc628b17
Gabriel Ambriz <angelgabrielda@hotmail.com>
2022-01-23 17:53:30 -0600

    33 - Singleton

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,26 +1,15 @@
 class MediaPlayer {
+  media: HTMLMediaElement;
+  plugins: Array<any>;
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [];
 
     this._initPlugins();
   }
-  _initPlugins() {
-    const player = {
-      play: () => this.play(),
-      pause: () => this.pause(),
-      media: this.media,
-      get muted() {
-        return this.media.muted;
-      },
-
-      set muted(value) {
-        this.media.muted = value;
-      },
-    };
-
+  private _initPlugins() {
     this.plugins.forEach((plugin) => {
-      plugin.run(player);
+      plugin.run(this);
     });
   }
   play() {
